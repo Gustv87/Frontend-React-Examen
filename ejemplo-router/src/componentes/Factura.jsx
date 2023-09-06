80cc5035d07c7103f1707581f6ffa88259766a22
ELVISfer <115681920+ELVISfer@users.noreply.github.com>
2023-09-05 22:09:27 -0600

    commit id_factura

--- ejemplo-router/src/componentes/Factura.jsx
+++ ejemplo-router/src/componentes/Factura.jsx
@@ -4,7 +4,6 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/esm/Container'
 
-
 var url  = "http://localhost:3000/api/factura";
 
 export const Factura = () => {
@@ -48,7 +47,7 @@ export const Factura = () => {
             </tr>
           </thead>
           <tbody>
-            { datos.map( x=> <tr key={x.id}><td>{x.id}</td><td>{x.correo}</td><td>{x.id_direccion}</td><td>{x.fecha}</td></tr>    ) }
+            { datos.map( x=> <tr key={x.id}><td>{x.id_factura}</td><td>{x.correo}</td><td>{x.id_direccion}</td><td>{x.fecha}</td></tr>    ) }
           </tbody>
         </Table></Col>
         </Row>

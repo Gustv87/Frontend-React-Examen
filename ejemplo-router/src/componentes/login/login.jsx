405289f500bce3119f1dc2e37a06e4df5375c09e
mariel2494 <dinorammontoya@hotmail.com>
2023-09-12 02:06:43 -0600

    Productos
    Css

--- ejemplo-router/src/componentes/login/login.jsx
+++ ejemplo-router/src/componentes/login/login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Login.css';
+
 
 export const Login = ( {dataSesion} ) => {
   const [correo, setCorreo] = useState('');
